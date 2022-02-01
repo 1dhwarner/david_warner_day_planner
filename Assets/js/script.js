63295c8a6761a6add2f3d7f194e174714f227e95
David Warner <david.hans.warner@gmail.com>
2022-02-01 13:41:57 -0700

    added index, js, and css files

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+// assign variables 
+var saveBtn = document.querySelector("save-icon");
+var hour9AM = document.querySelector("9AM");
+var hour10AM = document.querySelector("10AM");
+var hour11AM = document.querySelector("11AM");
+var hour12PM = document.querySelector("12PM");
+var hour1PM = document.querySelector("1PM");
+var hour2PM = document.querySelector("2PM");
+var hour3AM = document.querySelector("3PM");
+var hour4PM = document.querySelector("4PM");
+var hour5PM = document.querySelector("5PM");
+
+var fullSchedule = [hour9AM, hour10AM, hour11AM, hour12PM, hour1PM, hour2PM, hour3PM, hour4PM, hour5];
+
+// define functions 
+function updateTime 
+
+// special functions 
+
+
+// business logic 
